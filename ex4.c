f6411ef0b97a0b56b9545834455031c8adea3bd7
viicdemarco <143226948+viicdemarco@users.noreply.github.com>
2023-08-25 19:16:43 -0300

    aula 2

--- ex4.c
+++ ex4.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int a1, r, n, an, soma;
+    int n, a1, a2, a3, a4, a5, r;
     printf("\nDigite o primeiro termo (a1): ");
     scanf("%i", &a1);
     printf("\nDigite a razao (r): ");
     scanf("%i", &r);
-    printf("\nDigite o numero de termos (n): ");
-    scanf("%i", &n);
-    an = a1 + (n - 1) * r;
-    soma = (n * (a1 + an)) / 2;
-    printf("A soma da progressao aritmetica eh: %i\n", soma);
-
+    a2 = a1 + 2 * r; 
+    a3 = a2 + 2 * r;
+    a4 = a3 + 2 * r;
+    a5 = a4 + 2 * r;
+    
+    printf("a1 = %i\n", a1);
+    printf("a2 = %i\n", a2);
+    printf("a3 = %i\n", a3);
+    printf("a4 = %i\n", a4);
+    printf("a5 = %i\n", a5);
     return 0;
 }
 
